97a873907d1323418cfdf35b511d76699c9427a1
kanishkapan <147300802+kanishkapan@users.noreply.github.com>
2025-07-13 03:58:21 +0530

    touch ups

--- frontend/src/pages/AIROUTE.jsx
+++ frontend/src/pages/AIROUTE.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../components/LandingPage/Navbar/Navbar'
 import AIRouteMapLeaflet from '../components/AI route Optimizer/AIRouteMapLeaflet'
 import AIDashbaord from '../components/AI route Optimizer/AIDashbaord'
+import Deliverypartner from '../components/AI route Optimizer/Dileverypartner2'
 const AIROUTE = () => {
   return (
     <div>
 
         <Navbar/>
         <AIRouteMapLeaflet/>
-        <AIDashbaord/>
+        <Deliverypartner/>
     </div>
   )
 }
